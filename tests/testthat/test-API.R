2180b36073129607ac55c8fca9307ab9f796dd9c
Thomas Olsen <skrupsen@gmail.com>
2024-04-19 12:41:42 +0200

    update

--- tests/testthat/test-API.R
+++ tests/testthat/test-API.R
@@ -1,7 +1,7 @@
 test_that("set_api_key function sets the API key correctly", {
 
   # Generate a mocked API key with 51 characters
-  mocked_api_key <- paste(rep("x", 51), collapse = "")
+  mocked_api_key <- paste(rep("x", 56), collapse = "")
 
   # Mock the askpass::askpass function
   mockery::mock(askpass::askpass,
@@ -11,6 +11,6 @@ test_that("set_api_key function sets the API key correctly", {
   set_api_key()
 
   # Check if the length of the environment variable is set correctly
-  expect_equal(nchar(Sys.getenv("CHATGPT_KEY")), 51)
+  expect_equal(nchar(Sys.getenv("CHATGPT_KEY")), 56)
 
 })
