c27ea0e90445cb6ce045623e6baa7b47fd3364fd
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2025-07-02 12:09:01 +0200

    PR from fork test :rocket:

--- R/chat.R
+++ R/chat.R
@@ -1,4 +1,4 @@
-#' Chat with OpenAI's GPT-3.5-turbo model
+#' Chat with OpenAI's GPT-3.5-turbo model HEJ MED DIG
 #'
 #' This function takes a data frame of study details, constructs a prompt for each study, and sends it to OpenAI's GPT-3.5-turbo model for a response.
 #' The function returns a tibble with the original study details and the model's response.
@@ -11,7 +11,7 @@
 #'
 #' @import dplyr
 #' @export
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' prompt <- "Is this study about a Functional Family Therapy (FFT) intervention?"
@@ -24,7 +24,7 @@ chat <- function(data, prompt, model = "gpt-3.5-turbo") {
   set_api_key()
   base_url <- "https://api.openai.com/v1"
   api_key <- Sys.getenv("CHATGPT_KEY")
-  
+
   # Initialize an empty data frame to store the results
   results <- tibble::tibble(Title = character(), StudyID = character(), decision_gpt = character())
 
@@ -32,30 +32,30 @@ chat <- function(data, prompt, model = "gpt-3.5-turbo") {
   for (i in 1:nrow(data)) {
     # Extract the study details
     study_details <- paste(data[i, "title"], data[i, "abstract"], sep = " ")
-    
+
     # Create the user message
     user_message <- list(
       list(role = "user", content = paste0(prompt, " ", study_details))
     )
-    
+
     # Create the request body
     body <- list(model = model,
                  messages = user_message)
-    
+
     # Make the request
     req <- httr2::request(base_url)
     resp <-
-      req |> 
-      httr2::req_url_path_append("chat/completions") |> 
-      httr2::req_auth_bearer_token(token = api_key) |> 
-      httr2::req_headers("Content-Type" = "application/json") |> 
-      httr2::req_user_agent("Thomas Olsen") |> 
-      httr2::req_body_json(body) |> 
-      httr2::req_retry(max_tries = 4) |> 
-      httr2::req_throttle(rate = 15) |> 
-      httr2::req_perform() |> 
+      req |>
+      httr2::req_url_path_append("chat/completions") |>
+      httr2::req_auth_bearer_token(token = api_key) |>
+      httr2::req_headers("Content-Type" = "application/json") |>
+      httr2::req_user_agent("Thomas Olsen") |>
+      httr2::req_body_json(body) |>
+      httr2::req_retry(max_tries = 4) |>
+      httr2::req_throttle(rate = 15) |>
+      httr2::req_perform() |>
       httr2::resp_body_json()
-    
+
     # Extract the decision_gpt from the response
     decision_gpt <- resp$choices[[1]]$message$content
 
